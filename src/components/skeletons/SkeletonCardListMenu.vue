51369d34e0be2678f53bac1ab04ee4f28e4ee204
devcjlozano <carlosjlp@gmail.com>
2024-02-22 18:16:04 +0100

    feature: Loading and skeleton ListMenus.vue ✨

---
+++ src/components/skeletons/SkeletonCardListMenu.vue
@@ -0,0 +1,29 @@
+<script setup>
+defineProps({
+  numberRepeat: {
+    type: Number,
+    default: 1,
+  },
+  customClass: {
+    type: String,
+    default: "",
+  },
+});
+</script>
+
+<template>
+  <div
+    v-for="n in numberRepeat"
+    :key="n"
+    :class="[customClass, 'skeleton-card-menu']"
+  >
+    <q-skeleton height="150px" type="rect" />
+  </div>
+</template>
+
+<style scoped>
+.skeleton-card-menu {
+  margin-top: var(--spacing-md);
+  width: 100%;
+}
+</style>
--- src/pages/list-menus/ListMenus.vue
+++ src/pages/list-menus/ListMenus.vue
@@ -1,9 +1,12 @@
 <script setup>
 import { computed } from "vue";
 import { useMenusStore } from "src/store/useMenusStore";
+import { useStatePageStore } from "src/store/useStatePageStore";
+import SkeletonCardListMenu from "components/skeletons/SkeletonCardListMenu.vue";
 import CardListMenu from "./components/CardListMenu.vue";
 
 const menusStore = useMenusStore();
+const statePageStore = useStatePageStore();
 
 const menusComputed = computed(() => {
   return menusStore.menus;
@@ -15,19 +18,31 @@ const menusComputed = computed(() => {
     <span class="head-diary-food head-subtitle">Tus menús</span>
 
     <section class="page-my-diary-food">
-      <CardListMenu
-        class="list-menus__card-list"
-        v-for="menu in menusComputed"
-        :key="menu.id"
-        :menu="menu"
+      <SkeletonCardListMenu
+        custom-class="section_skeleton"
+        v-if="statePageStore.loadingMenus"
+        :number-repeat="4"
       />
+      <section class="section-list-menus__card-list" v-else>
+        <CardListMenu
+          v-for="menu in menusComputed"
+          :key="menu.id"
+          :menu="menu"
+        />
+      </section>
     </section>
   </div>
 </template>
 
 <style scoped>
-.list-menus__card-list {
-  margin-bottom: 20px;
+.section_skeleton,
+.section-list-menus__card-list {
   max-width: 1200px;
+}
+.section-list-menus__card-list {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  width: 100%;
 }
 </style>
